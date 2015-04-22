4e30642f695dad5cd8b9d8b923501138e7a34e59
Lisa Folkerson <lisafolkerson@gmail.com>
2015-04-22 10:02:40 -0400

    added structure for sass

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require ( 'gulp' );
+var sass = require ( 'gulp-sass' );
+var notify = require ( 'gulp-notify' );
+
+
+// Here we define the list of things to happen when we run gulp styles
+gulp.task( 'styles', function() {
+	gulp.src( 'wp-content/themes/LBF-Doored-Theme/' )
+	.pipe( sass({errLogToConsole : true}))
+	.pipe( gulp.dest('wp-content/themes/contemporary_art_gallery/' ))
+	.pipe( notify ( 'CSS Compiled' ));
+	});
+
+gulp.task( 'watch', function() {
+	gulp.watch('wp-content/themes/contemporary_art_gallery/*.scss',['styles'])
+	});
